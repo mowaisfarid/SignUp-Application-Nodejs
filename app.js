274973730e6e66f5206f09dfbc6324705fcda20c
owais farid <mowaisfarid@gmail.com>
2020-12-28 02:57:28 +0500

    some work required

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
-function  SignUp(){
+function signUp() {
+
+    let name = document.getElementById('names').value
+    let email = document.getElementById('emails').value
+    let password = document.getElementById('passwords').value
+    let gender = document.getElementById('genders').value
+    let phonenumber = document.getElementById('phonenumbers').value
+
+    let user = {
+        name: name,
+        email: email,
+        password: password,
+        gender: gender,
+        phonenumber: phonenumber
+    }
+    // console.log(user)
+
+
     const Http = new XMLHttpRequest();
-    // const url = 'https://192.168.50.177:3000/bulb';
-    const url = 'http://192.168.2.104:3000/bulb';
-    Http.open("GET", url);
-    Http.send();
+    const url = 'http://192.168.50.230:3000/signup';
+    // const url = 'https://legend-99.herokuapp.com/signup';
+    Http.open("POST", url);
+    // Http.send(user);
+    Http.setRequestHeader("Content-Type", "application/json");
+    // Http.send(JSON.stringify({ name: "John Rambo", time: "2pm" }));
+    Http.send(JSON.stringify(user));
 
     Http.onreadystatechange = (e) => {
         console.log(Http.responseText)
-        alert(Http.responseText)
-        // var result1 = Http.responseText
-        // document.getElementById("one").innerHTML = result1;
+        document.getElementById("show").innerHTML = Http.responseText;
+        // alert(Http.responseText)
+
     }
+    return false;
+
 }
 
-function Login(){
+function Login() {
+
+    let loginEmail = document.getElementById("userEmail").value;
+    let loginPassword = document.getElementById("userPassword").value;
+
+
     const Http = new XMLHttpRequest();
-    const url = 'http://192.168.2.104:3000/bulb';
-    // const url = 'http://192.168.50.177:3000/bulb';
+    // const url = 'http://192.168.40.60:3000/login';
+    const url = 'http://192.168.50.230:3000/login';
+    // const url = 'https://legend-99.herokuapp.com/login';
+
+
     Http.open("POST", url);
-    Http.send();
+    Http.setRequestHeader("Content-type", "application/json")
+
+    Http.send(JSON.stringify({
+        email: loginEmail,
+        password: loginPassword
+    }));
 
     Http.onreadystatechange = (e) => {
-        console.log(Http.responseText)
-        alert(Http.responseText)
+        console.log(Http.responseText);
+        document.getElementById("daashbord").innerHTML = Http.responseText;
+        // window.location.href = "profile.html"
 
-        
     }
+    return false;
 }
--- server.js
+++ server.js
@@ -2,14 +2,24 @@ let users = [];
 
 
 let express = require("express");
-
 var cors = require('cors')
 var morgan = require('morgan')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
+// const path=require("path");
+// const PORT=process.env.PORT || 3000;
+
+// const { json } = require("express");
+// const { response } = require("express");
 
 let app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+console.log(users);
 
 // app.use(function (req, res, next) {
 //     console.log("Method is: ", req.method)
@@ -20,30 +30,68 @@ app.use(cors());
 //     next();
 // });
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
 
-app.get("/bulb", function (req, res, next) {
-    res.send("bulb is On");
-});
 
-app.post("/bulb", function (req, res, next) {
+
+app.post("/signup", function (req, res, next) {
     
+    // res.send("Sign Up Successfully"+JSON.stringify(req.body));
+    // users.push(req.body)
+
+    console.log(req.body)
     console.log("body: ", req.body);
-    res.send("Sign Up Successfully");
-    // res.send("bulb is created");
+    // res.send("signUp successfully")
+
+    let alreadyExist = false;
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === req.body.email) {
+            alreadyExist = true;
+            break;
+        }
+    }
+    if (alreadyExist) {
+        res.send('This email address already exist')
+    } else {
+        users.push(req.body)
+        res.send("Sign Up Successfully");
+
+    }
 });
 
-app.put("/bulb", function (req, res, next) {
-    res.send("Bulb state is changed");
-});
+app.post("/signin", function (req, res, next) {
 
-app.delete("/bulb", function (req, res, next) {
-    res.send("bulb is deleted");
-});
+    // res.send("error")
 
+    let curUser = req.body.email;
+    let curPassword = req.body.password;
+    let isFound = false;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === curUser) {
+
+            isFound = i;
+            var newEmail = curUser;
+            break;
+        }
+    }
+    if (users[isFound].passwords === curPassword) {
+        // res.send("signed in succesfully" )
+        res.send(`signed in succesfully Name : ${users[isFound].name} Email : ${newEmail}
+        Gender : ${users[isFound].gender} Nationality : ${users[isFound].nationality}
+        Phone No : ${users[isFound].number}`)
+    }
+    else {
+        res.status(403).send("wrong password or email");
+    }
+
+
+})
 
 
 app.listen(3000, () => {
     console.log("server is running on 3000");
 })
+
+// app.listen(PORT || 5000 , () => {
+//     console.log("server is running on 5000");
+// })
